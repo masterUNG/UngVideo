6523829c632557448a33291d6cafd91c2b41c8d5
masterUNG <phrombutr@gmail.com>
2019-08-13 09:19:09 +0700

    Sign Out Success

---
+++ lib/screens/my_constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+  final String nameAppString = 'Ung Video';
+  final Color textColor = Colors.blueGrey[700];
+  final double h1 = 24.0;
+  final double h2 = 18.0;
+}
--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ung_video/screens/home.dart';
 
 class MyService extends StatefulWidget {
   @override
@@ -6,6 +8,104 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+  // Explicit
+  Color textColor = Colors.blueGrey.shade800;
+  String nameLogin = '';
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // Method
+  @override
+  void initState() {
+    super.initState();
+    findDisplayName();
+  }
+
+  Future<void> findDisplayName() async {
+    FirebaseUser firebaseUser = await firebaseAuth.currentUser();
+    setState(() {
+      nameLogin = firebaseUser.displayName;
+    });
+  }
+
+  Widget showLogin() {
+    return Text(
+      'Login by $nameLogin',
+      style: TextStyle(
+        fontStyle: FontStyle.italic,
+      ),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'Ung Video',
+      style: TextStyle(
+        fontSize: 18.0,
+        color: textColor,
+        fontFamily: 'BethEllen',
+      ),
+    );
+  }
+
+  Widget showLogo() {
+    return Container(
+      height: 60.0,
+      child: Image.asset(
+        'images/logo.png',
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  Widget myDrawerHead() {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          colors: [Colors.white, Colors.yellowAccent.shade700],
+          radius: 1.0,
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          showLogo(),
+          showAppName(),
+          showLogin(),
+        ],
+      ),
+    );
+  }
+
+  Widget signOutMenu() {
+    return ListTile(
+      leading: Icon(
+        Icons.import_export,
+        size: 36.0,
+      ),
+      title: Text('Sign Out'),onTap: (){
+        signOutProcess();
+        Navigator.of(context).pop();
+      },
+    );
+  }
+
+  Future<void> signOutProcess()async{
+    await firebaseAuth.signOut().then((response){
+      MaterialPageRoute materialPageRoute = MaterialPageRoute(builder: (BuildContext context) => Home());
+      Navigator.of(context).pushAndRemoveUntil(materialPageRoute, (Route<dynamic> route) => false);
+    });
+  }
+
+  Widget myDrawerMenu() {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          myDrawerHead(),
+          signOutMenu(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +113,7 @@ class _MyServiceState extends State<MyService> {
         title: Text('My Service'),
       ),
       body: Text('Body'),
+      drawer: myDrawerMenu(),
     );
   }
 }
